7d52736f07d9cf2ffeff64d7bc927ac822cc397a
Kishan-Das_ivanti <kishan.das@ivanti.com>
2025-05-19 12:30:03 +0530

    Added middleware

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const express = require('express');
+const bodyParser = require('body-parser'); 
+const cors = require('cors'); 
+const morgan = require('morgan'); // Added morgan
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors()); // Allow all origins for development
+app.use(morgan('dev')); // Log HTTP requests in development mode
 
 app.get('/', (req, res) => {
   res.send('Welcome to the Social Media App Backend!');
